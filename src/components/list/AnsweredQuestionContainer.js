f287e4989962727075efc084b56e04ea0a9a49e3
James Spence <Jamesaspence20@gmail.com>
2018-11-26 23:14:18 -0600

    consolidated questions components

--- src/components/list/AnsweredQuestionContainer.js
+++ src/components/list/AnsweredQuestionContainer.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-import Checklist from './Checklist';
+import ListItem from './AnsweredQuestion';
 
 const mapStateToProps = state => ({
   answeredQuestions: state.answeredQuestions
 });
 
-const AnsweredQuestionContainer = ({ answeredQuestions }) => (
-  <div className="answered-question-container half-height-container">
-    <Checklist questions={answeredQuestions} />
-  </div>
-);
+const AnsweredQuestionContainer = ({ answeredQuestions }) => {
+  return (
+    <div className="answered-question-container half-height-container">
+      {answeredQuestions.map((question, i) => (<ListItem key={i} question={question}/>))}
+    </div>
+  );
+};
 
 export default connect(mapStateToProps)(AnsweredQuestionContainer);
