7fb526d45ab2ad8223915379d9dae76f733f9b58
James Spence <Jamesaspence20@gmail.com>
2019-01-26 14:10:06 -0600

    disabled registration form temporarily

--- src/components/auth/form/RegistrationForm.js
+++ src/components/auth/form/RegistrationForm.js
@@ -3,17 +3,20 @@ import { SWITCH_REGISTRATION } from '../Switcher';
 import AuthForm from './AuthForm';
 import FormInput from './FormInput';
 
-const RegistrationForm = ({ switchForm }) => (
-  <AuthForm
-    header="Registration"
-    active={switchForm === SWITCH_REGISTRATION}>
+
+/*<AuthForm
+  header="Registration"
+  active={switchForm === SWITCH_REGISTRATION}>
     <FormInput type="text" name="first_name" placeholder="First Name" />
     <FormInput type="text" name="last_name" placeholder="Last Name" />
     <FormInput type="text" name="email" placeholder="Email" />
     <FormInput type="text" name="confirm_email" placeholder="Email Confirmation" />
     <FormInput type="password" name="password" placeholder="Password" />
     <FormInput type="password" name="confirm_password" placeholder="Password Confirmation" />
-  </AuthForm>
+</AuthForm>*/
+
+const RegistrationForm = ({ switchForm }) => (
+  <div className="auth-form">Hello</div>
 );
 
 export default RegistrationForm;
