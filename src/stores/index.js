95421457185b688923bed115753710367a4d348a
James Spence <Jamesaspence20@gmail.com>
2019-01-26 15:09:40 -0600

    redux thunk now enabled

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(reduxThunk)
+  )
+);
