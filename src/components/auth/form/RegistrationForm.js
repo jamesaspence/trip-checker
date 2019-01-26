da326a3d9ebcd7f2948b364ff126b67105ec24f2
James Spence <Jamesaspence20@gmail.com>
2019-01-26 15:04:30 -0600

    added required fields

--- src/components/auth/form/RegistrationForm.js
+++ src/components/auth/form/RegistrationForm.js
@@ -13,11 +13,12 @@ class RegistrationForm extends Component {
     return (
       <AuthForm
         header="Registration"
-        active={active}>
+        active={active}
+        requiredFields={["first_name", "last_name", "email", "confirm_email", "password", "confirm_password"]}>
         <FormInput type="text" name="first_name" placeholder="First Name" autoComplete="given-name" required />
         <FormInput type="text" name="last_name" placeholder="Last Name" autoComplete="family-name" required />
-        <FormInput type="text" name="email" placeholder="Email" autoComplete="email" required />
-        <FormInput type="text" name="confirm_email" placeholder="Email Confirmation" autoComplete="email" required />
+        <FormInput type="email" name="email" placeholder="Email" autoComplete="email" required />
+        <FormInput type="email" name="confirm_email" placeholder="Email Confirmation" autoComplete="email" required />
         <FormInput type="password" name="password" placeholder="Password" autoComplete="new-password" required />
         <FormInput type="password" name="confirm_password" placeholder="Password Confirmation" autoComplete="new-password" required />
       </AuthForm>
