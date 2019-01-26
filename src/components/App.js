5dcec946c712dbe176455ea94777723d74e7b35e
James Spence <Jamesaspence20@gmail.com>
2019-01-26 10:53:44 -0600

    moving of base styles into app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Component } from 'react';
 import PackContainer from './packing/PackContainer';
+import AuthContainer from './auth/AuthContainer';
+
+import './App.scss';
 
 class App extends Component {
   render () {
