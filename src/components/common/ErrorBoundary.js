f5dc8a3b2403cdb4b8a78176b6ae02b47a54609b
James Spence <Jamesaspence20@gmail.com>
2019-11-13 22:32:19 -0600

    small fixes in error boundary
    Added link to home page to reset app, missing semicolon

--- src/components/common/ErrorBoundary.js
+++ src/components/common/ErrorBoundary.js
@@ -25,14 +25,14 @@ class ErrorBoundary extends Component {
         <h1 className="error-header"><span role="img" aria-label="grimacing">😬</span> OH NO <span role="img" aria-label="grimacing">😬</span></h1>
         <p className="error-message">Something unexpected went wrong. We must've crossed the i's and dotted the t's.</p>
         <p className="error-message">Terribly sorry for the inconvenience. Please try again later.</p>
-        <button type="button" className="error-link">It's okay, I'm ready to trust you again (resets app)</button>
+        <a href="/" className="error-link">It's okay, I'm ready to trust you again (resets app)</a>
       </div>
     )
   }
 
   render() {
     const { children } = this.props;
-    const { error } = this.state
+    const { error } = this.state;
     if (typeof error !== 'undefined') {
       return this.renderError();
     }
