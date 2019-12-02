145e346f2ba7c51138d7fceaa3b2ca7206f5b9b1
James Spence <Jamesaspence20@gmail.com>
2019-12-01 23:22:49 -0600

    added link to header

--- src/components/common/AppBar.js
+++ src/components/common/AppBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './AppBar.scss';
+import { Link } from 'react-router-dom';
+import { HOME } from '../../util/routes';
 
 const AppBar = () => (
   <div className="app-bar">
-    <h1 className="app-bar-header">Packit</h1>
+    <Link className="app-link" to={HOME}><h1 className="app-bar-header">Packit</h1></Link>
   </div>
 );
 
